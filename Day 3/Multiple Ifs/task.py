3ee2941f931ea7661e9d90980036beef22f00d0a
Witek G <witold@grzegorczyk.it>
2026-07-07 18:41:43 +0200

    Multiple Ifs

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -4,11 +4,18 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
+    bill = 0
     if age <= 12:
         print("Please pay $5.")
+        bill += 5
     elif age <= 18:
         print("Please pay $7.")
+        bill += 7
     else:
         print("Please pay $12.")
+        bill += 12
+    want_photo = input("Do you want a photo? (Y/N) ")
+    if want_photo == "Y" or want_photo == "y": bill += 3
+    print(f"The total bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
